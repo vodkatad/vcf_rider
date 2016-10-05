8215b20a2ddfc1119b51d75524d450e9f72e7481
Elena Grassi <grassi.e@gmail.com>
2016-10-05 12:24:02 +0200

    Converted .ok.expect() to corresponding match
    (hopefully corresponding, even if the e Error reported by the
    Record Results is still unknown to me).

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate bio;
 mod tests {
     use bio::io::bed;
 
+    // constant named BED_FILE of type pointer to vec of u8 with a 'static lifetime.
     const BED_FILE: &'static [u8] = b"1\t5\t5000\tname1\tup
 2\t3\t5005\tname2\tup
 ";
@@ -17,10 +18,27 @@ mod tests {
         let names = ["name1", "name2"];
         let scores = ["up", "up"];*/
 
+        // reader needs to be mut because TODO.
         let mut reader = bed::Reader::new(BED_FILE);
         for r in reader.records() {
-            let record = r.ok().expect("Error reading record");
-            println!("bed name: {}", record.name().expect("Error reading name"))
+            // .ok().expect() explanation:
+            // Option<&str> is returned by record.name(). We could have used a match (TODO).
+            // ok works on results, and records is an Iterator of Result<Record>
+            // expect unwraps an option, yielding the content of a Some, the given &str is
+            // the custom panic message if a None is there instead.
+
+            // We rewrite the original lines as matches.
+            // let record = r.ok().expect("Error reading record");
+            let record = match r {
+                Ok(x) => x,
+                Err(e) => panic!("Error reading record {}", e),
+            };
+            //println!("bed name: {}", record.name().expect("Error reading name"))
+            let name = match record.name() {
+                Some(x) => x,
+                None => panic!("Error reading name"),
+            };
+            println!("bed name: {}", name)
             /*assert_eq!(record.chrom(), chroms[i]);
             assert_eq!(record.start(), starts[i]);
             assert_eq!(record.end(), ends[i]);
