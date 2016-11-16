6439fd4fb31d77a1cca9a7e5dcca381e186c0bd8
Elena Grassi <grassi.e@gmail.com>
2016-11-16 09:18:57 +0100

    Filtering unphased and multiallelic entries

--- src/mutations.rs
+++ src/mutations.rs
@@ -88,7 +88,7 @@ fn decode_allele(c: char) -> bool {
     }
 }
 
-fn decode_genotype(geno: String) -> (bool, bool) {
+fn decode_genotype(geno: String, phased: &mut bool) -> (bool, bool) {
     // this will need a lot of work inside the lib
     // here genotypes[s] is the str (?) 0|0, ok. These are displayable, check their code to understand the inner structure.
     // https://github.com/rust-bio/rust-htslib/blob/master/src/bcf/record.rs
@@ -100,7 +100,7 @@ fn decode_genotype(geno: String) -> (bool, bool) {
     let sep = genos.next().unwrap();
     let a2 = genos.next().unwrap();
     if sep != '|' {
-        panic!("Only phased genotypes are supported! {}", sep)
+        *phased = false;
     }
     (decode_allele(a1),decode_allele(a2))
 }
@@ -110,36 +110,44 @@ impl Iterator for VcfReader {
 
     fn next(&mut self) -> Option<Mutation> {
         let mut record : bcf::Record = bcf::Record::new();
-        if let Ok(_) = self.reader.read(&mut record) {
-            let id = "".to_owned();
-            let chr = "?".to_owned(); // where is it? rid?
-            let coord = record.pos() as u64; // Manually checked: the lib converts 1 based coords of vcf to 0 based. bed records have u64
-            let alleles = record.alleles().into_iter().map(|a| a.to_owned()).collect_vec(); // I do not like this to_owned...
-            let refe : Vec<u8> = get_sequence(&alleles[0]);
-            let mut alt  : Vec<u8> = Vec::<u8>::with_capacity(1);
-            let mut found_alt = 0;
-            for allele in alleles[1..].iter() { // why skip the first? The first is the reference. Then are listed all the alternative ones.
-                //println!("allele {}" , allele[0] as char); // this is the alt allele? Why are they vectors? Because they are encoded as single bases.
-                alt.push(allele[0] as u8);
-                found_alt += 1;
-                //println!("allele {}" , allele[1] as char); // this will print the second base if the alt allele is for example AC (C).
-            }
-            if found_alt != 1 {
-                panic!("Cannot manage multi-allelic SNPs! {:?}", alt) // maybe simply skip?
-            }
-            let alte = get_sequence(&alt);
-            // remember trim_alleles(&mut self)
+        loop {
+            if let Ok(_) = self.reader.read(&mut record) {
+                let id = "".to_owned();
+                let chr = "?".to_owned(); // where is it? rid?
+                let coord = record.pos() as u64; // Manually checked: the lib converts 1 based coords of vcf to 0 based. bed records have u64
+                let alleles = record.alleles().into_iter().map(|a| a.to_owned()).collect_vec(); // I do not like this to_owned...
+                let refe : Vec<u8> = get_sequence(&alleles[0]);
+                let mut alt  : Vec<u8> = Vec::<u8>::with_capacity(1);
+                let mut found_alt = 0;
+                for allele in alleles[1..].iter() { // why skip the first? The first is the reference. Then are listed all the alternative ones.
+                    //println!("allele {}" , allele[0] as char); // this is the alt allele? Why are they vectors? Because they are encoded as single bases.
+                    alt.push(allele[0] as u8);
+                    found_alt += 1;
+                    //println!("allele {}" , allele[1] as char); // this will print the second base if the alt allele is for example AC (C).
+                }
+                if found_alt != 1 {
+                    // print something on stderr
+                    continue;
+                }
+                let alte = get_sequence(&alt);
+                // remember trim_alleles(&mut self)
 
-            let rgenotypes = record.genotypes().unwrap();
-            let genotypes = (0..self.reader.header.sample_count() as usize).map(|s| {
-                                let geno_str = format!("{}", rgenotypes.get(s));
-                                decode_genotype(geno_str)
-                            }).collect_vec(); //useful to pre alloc?  Vec::<(bool, bool)>::with_capacity(self.samples.len());
- 
-            let pos = Coordinate { chr: chr, start: coord, end: coord+1}; // Right now only Snps.
-            Some(Mutation { id: id, pos: pos, sequence_ref: refe, sequence_alt: alte, genotypes : genotypes})
-        } else {
-            return None;
+                let mut phased = true;
+                let rgenotypes = record.genotypes().unwrap();
+                let genotypes = (0..self.reader.header.sample_count() as usize).map(|s| {
+                                    let geno_str = format!("{}", rgenotypes.get(s));
+                                    decode_genotype(geno_str, & mut phased)
+                                }).collect_vec(); //useful to pre alloc?  Vec::<(bool, bool)>::with_capacity(self.samples.len());
+                // cannot break inside of a closure therefore we continue here if there were some samples not phased.
+                // inefficient because it will decode all genotypes 
+                if ! phased {
+                    continue;
+                }
+                let pos = Coordinate { chr: chr, start: coord, end: coord+1}; // Right now only Snps.
+                return Some(Mutation { id: id, pos: pos, sequence_ref: refe, sequence_alt: alte, genotypes : genotypes})
+            } else {
+                return None;
+            }
         }
     }
 }
