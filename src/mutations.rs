610bf8eead76a5c4ddc9f2cb96b51d7bd49ac939
Elena Grassi <grassi.e@gmail.com>
2017-05-04 11:48:06 +0200

    Debugging run

--- src/mutations.rs
+++ src/mutations.rs
@@ -132,10 +132,10 @@ fn decode_genotype(geno: String, accept_phased: bool) -> (bool, bool) {
     let mut genos = geno.chars();
     let a1 = genos.next().unwrap();
     if a1 == '.' {
-        return (true, true) // missing data as reference.
+        return (false, false) // missing data as reference. BUG! false is reference, true is alternative!
     }
     let sep = genos.next().unwrap();
-    let a2 = genos.next().unwrap();
+    let a2 = genos.next().unwrap(); 
     if sep != '|' && !accept_phased {
         panic!("Only phased genotypes are supported! {}", sep)
     }
