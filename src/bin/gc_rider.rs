5cec8797f097054f887b24c1eb106c011165e5d1
Elena Grassi <grassi.e@gmail.com>
2018-03-02 13:48:29 +0100

    gc_rider
    Copied code from gc_counter since I hate mod/use/crate and do not get them.

--- src/bin/gc_rider.rs
+++ src/bin/gc_rider.rs
@@ -1,14 +1,53 @@
 extern crate vcf_rider;
 extern crate bio;
 extern crate argparse;
+//extern crate gc_counter;
 
 use vcf_rider::rider::*;
 use bio::io::bed;
 use std::path::Path;
 use argparse::{ArgumentParser, Store};
-use std::io::BufReader;
-use std::fs::File;
-use std::io::BufRead;
+//use gc_counter::{G, C, Counter};
+//mod gc_counter;
+use std::collections::hash_map::HashMap;
+
+
+const C: u8 = 1;
+const G: u8 = 2;
+
+pub struct Counter {
+    pub nucleos: HashMap<u8, u64>,
+    pub name: String
+}
+impl Counter {
+    pub fn new(to_count: Vec<u8>, name: &str) -> Counter {
+        let mut nucleos : HashMap<u8, u64> = HashMap::new();
+        for n in to_count {
+            nucleos.insert(n, 0u64);
+        }
+        Counter { nucleos: nucleos, name: name.to_owned()}
+    }
+}
+
+impl CanScoreSequence for Counter {
+    fn get_length(&self) -> usize {
+        1
+    }
+
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    fn get_score(&self, pos: usize, sequence: &[u8]) -> f64 {
+        // we could also keep counts for single nucleotide inside Counter in this case.
+        if self.nucleos.contains_key(&sequence[pos]) {
+            1f64
+        } else {
+            0f64
+        }
+    }
+}
+
 
 // To obtain GC counts for different individuals on regions of interest.
 fn main() {
@@ -36,8 +75,12 @@ fn main() {
     // reference sequence and an optional file where it will print bed entries with their overlapping snps (only for entries with at least one overlapping SNP.)
     // In the end it is not good to print directly results inside the library so it will return an appropriate data structure with results that will be printed here.
     // Right now for our pipelines it is ok to print inside it.
+    let gc_counter = Counter::new(vec!(C, G), &"CG");
+    let mut vec : Vec<Counter> = Vec::new();
+    vec.push(gc_counter);
+
     if let Ok(bed_reader) = bed::Reader::from_file(Path::new(&bed_filename)) {
-        get_scores(RiderParameters {min_len: 1, max_len: 1, parameters: &matrixes}, &vcf_filename, bed_reader, &ref_filename, assoc_file_opt, true);
+        get_scores(RiderParameters {min_len: 1, max_len: 1, parameters: &vec}, &vcf_filename, bed_reader, &ref_filename, assoc_file_opt, false);
     } else {
         panic!("Could not open bed file!");
     }
